60e5f7bb358b89eaf92e41a290e3093181e6f8ba
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-01-06 22:08:50 -0500

    Able to render a 15 character long random password

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let passwordOne = "";
+let passwordTwo = "";
+let passwordOneEl = document.getElementById("password-one");
+let passwordTwoEl = document.getElementById("password-two");
 
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * characters.length)
+    return randomNumber;
+}
+
+function renderPassword() {
+    for(let i = 0; i < 15; i++){
+        passwordOne += characters[getRandomNumber()];
+    }
+    console.log(passwordOne)
+}
+
+renderPassword();
