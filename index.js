7b53430a1cf042808cd3dc5f4763272bd80ef0c3
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-01-10 14:29:01 -0500

    Fixed an issue where the generate password button would keep adding on characters everytime it was pressed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordOne = "";
-let passwordTwo = "";
+
 let passwordOneEl = document.getElementById("password-one");
 let passwordTwoEl = document.getElementById("password-two");
+let passwordLengthEl = document.getElementById("password-length");
+
+//Use this function to get the form input information and apply it to the password output
+function getPasswordLength() {
+
+}
 
 function getRandomNumber() {
     let randomNumber = Math.floor(Math.random() * characters.length)
@@ -11,6 +16,9 @@ function getRandomNumber() {
 }
 
 function renderPassword() {
+    let passwordOne = "";
+    let passwordTwo = "";
+    
     for(let i = 0; i < 15; i++){
         passwordOne += characters[getRandomNumber()];
         passwordTwo += characters[getRandomNumber()];
