acd762164f45680ddfee20f0aa86e566b77bbbe2
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-03-27 20:13:24 -0400

    Have a possible function to filter out symbols and numbers

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import { characters } from "./characters.js"
 const passwordBtn = document.querySelector('.generate-password-button')
+const isSymbols = document.getElementById('symbols').checked 
+const isNumbers = document.getElementById('numbers').checked
+
 
 
 //Event Listeners
 passwordBtn.addEventListener('click', renderPassword)
+document.addEventListener('click', copyOnClick)
 
 
 //Creating a password by first rendering a random character and then using a loop to create a string for the full password
 function renderPassword() {
     const passwords = document.querySelectorAll('.password-one, .password-two')
-
     passwords.forEach(password => {
         password.textContent = '';
     })
-
     passwords.forEach(password => {
         for(let i = 0; i <= 17; i++){
             password.textContent += renderCharacter()
@@ -21,26 +23,33 @@ function renderPassword() {
     })
 }
 
+
 function renderCharacter() {
     let randomNumber = Math.floor(Math.random() * characters.length)
     let randomChar = characters[randomNumber]
     return randomChar
 }
 
+function filterSymbols(){
+    const noSymbolsArray = characters.filter(item => {
+        if(characters.indexOf(item) >= 0 && characters.indexOf(item) <= 61){
+            return item;
+        }
+    })
+}
+
 
 
 //Copy-on-click
 //Want to figure out how to just focus on the password container and not use document for this
-document.addEventListener('click', copyOnClick)
-
 function copyOnClick(e){
     const passwordToCopy = document.querySelector(`.${e.target.className}`)
     const copiedMessage = document.querySelector(`.${e.target.className}-copied`)
 
     navigator.clipboard.writeText(passwordToCopy.textContent)
     copiedMessage.classList.add('show-copied')
-
     setTimeout(function() {
         copiedMessage.classList.remove('show-copied')
     }, 1500)
 }
+
